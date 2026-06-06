da6698de28e41e7d483431105fa83ca743ac47ac
rockgaidencmd <rockgaiden@gmail.com>
2026-06-05 21:49:07 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
   });
 }
