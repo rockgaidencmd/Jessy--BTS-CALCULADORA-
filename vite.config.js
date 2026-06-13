f91f5af81b8df6435988c04a1b883c24e5abe813
rockgaidencmd <rockgaiden@gmail.com>
2026-06-12 23:12:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Jessy--BTS-CALCULADORA-/',
+  base: '/',
   plugins: [react()],
 })
